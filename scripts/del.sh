a5e55a7165ee7f6c08fcace6230e4b26b6d6fe5b
Punit Choudhary <haxor@Punits-M4-Air.local>
2025-04-28 12:40:36 +0530

    first commit

---
+++ scripts/del.sh
@@ -0,0 +1,16 @@
+function del() {
+  if [[ $# -eq 0 ]]; then
+    echo "Usage: del <file_or_folder>"
+    return 1
+  fi
+  for item in "$@"; do
+    if [ ! -e "$item" ]; then
+      echo "Error: '$item' does not exist."
+      continue
+    fi
+
+    if ! osascript -e "tell application \"Finder\" to delete POSIX file \"$(realpath "$item")\"" >/dev/null; then
+      echo "Failed to delete: $item"
+    fi
+  done
+}
